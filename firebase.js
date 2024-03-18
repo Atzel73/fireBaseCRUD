b1a1dd5f3e0e91dbabbd3dfe846c4c555018d038
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-18 16:56:42 -0600

    firebase script and index.js fulled

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  import {  } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAdzl-HhOVAAhSJE4CTxYjdO7toEbT62U0",
+    authDomain: "crud-569a6.firebaseapp.com",
+    projectId: "crud-569a6",
+    storageBucket: "crud-569a6.appspot.com",
+    messagingSenderId: "857019519989",
+    appId: "1:857019519989:web:0a6a80a9047092cdfe46bc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Works!");
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("works!");
+})
+
+const taskForm =   document.getElementById("task-form");
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    console.log("submitted");
+
+    const title = taskForm['task-title'];
+    const description = taskForm['task-description'];
+
+    
+
+})
